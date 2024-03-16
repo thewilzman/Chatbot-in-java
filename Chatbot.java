41217938eb122442479660abf79e1942efab1854
thewilzman <124735016+thewilzman@users.noreply.github.com>
2024-03-15 21:21:29 -0400

    Add files via upload

---
+++ Chatbot.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Chatbot {
+    public static void main(String[] args) {
+        
+        //set up scanner
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello. What is your name?");
+        //Pick up user's name and store it. 
+        String userName = scan.nextLine();
+        
+
+                           
+        //add new a line before asking next question. 
+        
+        System.out.println("\nHi " + userName+  "! I'm Javabot. Where are you from?");
+        //Pick up user's home and store it. 
+        String home = scan.nextLine();
+        
+                        
+
+        //add new a line before asking next question.
+        System.out.println("\nI hear it's beautiful at" + home + "! I'm from a place called Oracle");
+        System.out.println("How old are you?");
+        //Pick up age and store it.
+
+        int age = scan.nextInt();
+        
+
+
+        //add new a line before asking next question.
+        scan.nextLine();
+        System.out.println("\nSo you're " + age +  ", cool! I'm 400 years old.");
+        System.out.println("This means I'm " + 400/28 +  " times older than you.");
+        System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
+        //Pick up language and store it. 
+        String language = scan.nextLine();
+
+
+
+        //add new a line here.
+        System.out.println(language + ", that's great! Nice chatting with you "+ userName + ". I have to log off now. See ya!");
+        
+        //close scanner. 
+        scan.close();
+
+        
+    }
+}
